a510cdd034fb494f4236b500d8452dcc4e4153b5
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-03-13 17:29:50 +0100

    style navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,14 @@
+import logoWebsite from '../assets/logoauto.png'
+
 const Navbar = () => {
   return (
     <nav className="w-full bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto ">
         <a
-          href="https://flowbite.com/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
+          href="/"
+          className="flex items-center justify-baseline space-x-3 rtl:space-x-reverse"
         >
-          <img src="/assets/logo.png" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
+          <img src={logoWebsite} className="w-35  h-20" alt="Flowbite Logo" />
         </a>
         <div className="flex md:order-2">
           <button
@@ -17,7 +16,7 @@ const Navbar = () => {
             data-collapse-toggle="navbar-search"
             aria-controls="navbar-search"
             aria-expanded="false"
-            className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
+            className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700  rounded-lg text-sm p-2.5 me-1"
           >
             <svg
               className="w-5 h-5"
@@ -40,7 +39,7 @@ const Navbar = () => {
           <button
             id="theme-toggle"
             type="button"
-            className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5"
+            className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-3xl p-1 px-3 "
           >
             <svg
               id="theme-toggle-dark-icon"
@@ -66,7 +65,7 @@ const Navbar = () => {
             </svg>
           </button>
 
-          <div className="relative hidden md:block">
+          <div className="relative hidden md:block ml-3">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg
                 className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -89,14 +88,21 @@ const Navbar = () => {
             <input
               type="text"
               id="search-navbar"
-              className="block w-40 p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="stop block w-40 p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white h-10 focus:ring-2 focus:ring-[#22881b] "
               placeholder="Search..."
             />
           </div>
           <button
+            type="button"
+            class="text-green-500 bg-blue-700 ring-2 hover:bg-blue-800 font-medium rounded-lg text-sm px-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700  ml-3"
+          >
+            Accedi
+          </button>
+
+          <button
             data-collapse-toggle="navbar-search"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100  dark:text-gray-400 dark:hover:bg-gray-700 "
             aria-controls="navbar-search"
             aria-expanded="false"
           >
@@ -145,7 +151,7 @@ const Navbar = () => {
             <input
               type="text"
               id="search-navbar"
-              className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="stop block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
               placeholder="Search..."
             />
           </div>
@@ -154,10 +160,10 @@ const Navbar = () => {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:color-website md:p-0 md:dark:hover:color-website dark:text-white dark:hover:color-website dark:hover:color-website md:dark:hover:color-website dark:border-gray-700"
                 aria-current="page"
               >
-                Home
+                Auto
               </a>
             </li>
             <li>
@@ -165,7 +171,7 @@ const Navbar = () => {
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                About
+                Preferiti
               </a>
             </li>
             <li>
@@ -173,7 +179,15 @@ const Navbar = () => {
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Services
+                Brand
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Nazioni
               </a>
             </li>
           </ul>
